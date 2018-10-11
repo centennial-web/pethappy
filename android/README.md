853e24269f45fc653a7aac72990a509d6783518c
Rodrigo Garcia <rodgarcialima@gmail.com>
2018-10-10 20:55:29 -0400

    Screen shot

---
+++ android/README.md
@@ -0,0 +1,1 @@
+### Local Proxy: `https://www.ngrok.com`
--- android/app/src/main/java/ca/petsuppliesathome/android/Consts.java
+++ android/app/src/main/java/ca/petsuppliesathome/android/Consts.java
@@ -2,5 +2,5 @@ package ca.petsuppliesathome.android;
 
 public class Consts {
 //    public static final String URL_BASE = "http://10.24.64.86:8090";
-    public static final String URL_BASE = "http://10.24.64.200:8090";
+    public static final String URL_BASE = "http://d621c726.ngrok.io";
 }
