026554924fbc143448c0e18bc0c657154e3ecc71
Rodrigo Garcia <rodgarcialima@gmail.com>
2018-11-15 15:32:48 -0500

    Data update

--- server/src/main/java/ca/pethappy/server/repositories/CategoriesRepo.java
+++ server/src/main/java/ca/pethappy/server/repositories/CategoriesRepo.java
@@ -3,10 +3,11 @@ package ca.pethappy.server.repositories;
 import ca.pethappy.server.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RepositoryRestResource(collectionResourceRel = "categories", path = "categories")
+@RepositoryRestResource(collectionResourceRel = "categories",  path = "categories")
 public interface CategoriesRepo extends JpaRepository<Category, Long> {
     Category findByName(String name);
 }
--- server/src/main/java/ca/pethappy/server/repositories/ProductsRepo.java
+++ server/src/main/java/ca/pethappy/server/repositories/ProductsRepo.java
@@ -2,10 +2,11 @@ package ca.pethappy.server.repositories;
 
 import ca.pethappy.server.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.Description;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RepositoryRestResource(collectionResourceRel = "products", path = "products")
+@RepositoryRestResource(path = "products", collectionResourceRel = "products")
 public interface ProductsRepo extends JpaRepository<Product, Long> {
 }
