a09477bb957452629b77ffa09aea7fee658a0da6
Rodrigo Garcia <rodgarcialima@gmail.com>
2018-11-16 15:26:32 -0500

    GitIgnore changes

--- android/app/src/main/java/ca/pethappy/pethappy/android/consts/Consts.java
+++ android/app/src/main/java/ca/pethappy/pethappy/android/consts/Consts.java
@@ -1,7 +1,8 @@
 package ca.pethappy.pethappy.android.consts;
 
 public final class Consts {
-    public static final String SERVER_URL = "http://66273e4f.ngrok.io/";
+//    public static final String SERVER_URL = "http://66273e4f.ngrok.io/";
+    public static final String SERVER_URL = "http://9491325b.ngrok.io/"; // rodrigo
     public static final String GUEST_USER = "guest@pethappy.ca";
     public static final String GUEST_PASSWORD = "mz89F#@";
 }
