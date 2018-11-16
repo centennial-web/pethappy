8a4b0caabe0d153ef2087a0496620e17b08c9036
Saurabhdeep Bains <saurabhbains919@gmail.com>
2018-11-15 22:37:16 -0500

    Added Bottom Nav to Android

--- android/app/src/main/java/ca/pethappy/pethappy/android/consts/Consts.java
+++ android/app/src/main/java/ca/pethappy/pethappy/android/consts/Consts.java
@@ -1,7 +1,7 @@
 package ca.pethappy.pethappy.android.consts;
 
 public final class Consts {
-    public static final String SERVER_URL = "http://ed0347d4.ngrok.io/";
+    public static final String SERVER_URL = "http://66273e4f.ngrok.io/";
     public static final String GUEST_USER = "guest@pethappy.ca";
     public static final String GUEST_PASSWORD = "mz89F#@";
 }
