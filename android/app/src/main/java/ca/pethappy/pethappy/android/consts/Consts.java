8a441e516353cde04f6c606a467dde00c2eabaaf
Saurabhdeep Bains <saurabhbains919@gmail.com>
2018-11-19 19:54:28 -0500

    Product Details Activity

--- android/app/src/main/java/ca/pethappy/pethappy/android/consts/Consts.java
+++ android/app/src/main/java/ca/pethappy/pethappy/android/consts/Consts.java
@@ -1,5 +1,5 @@
 package ca.pethappy.pethappy.android.consts;
 
 public final class Consts {
-    public static final String SERVER_URL = "http://afbb4042.ngrok.io";
+    public static final String SERVER_URL = "http://e1f87482.ngrok.io";
 }
